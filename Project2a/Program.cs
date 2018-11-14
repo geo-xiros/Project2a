e97ba0ad7c14bf00d044a575f4a883d97a2f68d3
George Xiros <geo.xiros@gmail.com>
2018-11-14 07:19:07 +0200

    Remove geting numbers and calculation from Program to methods

--- Project2a/Program.cs
+++ Project2a/Program.cs
@@ -10,31 +10,49 @@ namespace Project2a
     {
         static void Main(string[] args)
         {
+            int result1 = GetTwoNumbersCalculation();
+            Console.WriteLine(result1);
+            int result2 = GetTwoNumbersCalculation();
+            Console.WriteLine(result2);
+            Console.ReadLine();
+
+        }
+        public static int GetTwoNumbersCalculation()
+        {
             UserInputNumber userInputNumber = new UserInputNumber();
             UserInputOperator userInputOperator = new UserInputOperator();
 
-            int number1;
-            while (!userInputNumber.GetInputNumber("Get First Number:", out number1))
+            int number1 = GetANumber(userInputNumber, "Give First Number:");
+            int number2 = GetANumber(userInputNumber, "Give Second Number:");
+            string calculateOperator = GetAnOperator(userInputOperator, $"Get calculation operator ({Calculator.Operators()}):");
+
+            while (calculateOperator == "/" && number2 == 0)
+            {
+                Console.WriteLine("Can not divide by zero.");
+                number2 = GetANumber(userInputNumber, "Give a Second Number :");
+            }
+
+            return Calculator.Calculate(calculateOperator, number1, number2);
+
+        }
+        static int GetANumber(UserInputNumber userInputNumber, string inputMessage)
+        {
+            int number;
+            while (!userInputNumber.GetInputNumber(inputMessage, out number))
             {
                 Console.WriteLine("Invalid number !!!");
             }
-
-            int number2;
-            while (!userInputNumber.GetInputNumber("Get Second Number:", out number2))
-            {
-                Console.WriteLine("Invalid number !!!");
-            }
-
+            return number;
+        }
+        static string GetAnOperator(UserInputOperator userInputOperator, string inputMessage)
+        {
             string calculateOperator;
-            while (!userInputOperator.GetInputOperator($"Get calculation operator ({Calculator.Operators()}):", out calculateOperator) ||
+            while (!userInputOperator.GetInputOperator(inputMessage, out calculateOperator) ||
                    !Calculator.IsValidOperator(calculateOperator))
             {
                 Console.WriteLine("Invalid operator !!!");
             }
-
-            Console.WriteLine(Calculator.Calculate(calculateOperator, number1, number2));
-            Console.ReadLine();
-
+            return calculateOperator;
         }
     }
 }
