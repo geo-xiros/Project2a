62c44215e35ef2f7ade7d107690682a835a50ceb
george <geo.xiros@gmail.com>
2018-11-14 20:33:26 +0200

    Add MyMath...

--- Project2a/MyMath.cs
+++ Project2a/MyMath.cs
@@ -10,15 +10,53 @@ namespace Project2a
     {
         static public int Fibonacci(int number)
         {
-            throw new NotImplementedException();
+            if (number == 0) return 0;
+            if (number < 2) return 1;
+            return Fibonacci(number - 1) + Fibonacci(number - 2);
         }
-        static public int GreatestCommonDivisor(int number)
+        static public int GreatestCommonDivisor(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 == 0)
+            {
+                throw new ArgumentOutOfRangeException("number1", "Parameters can not be zero.");
+            }
+
+            if (number2 == 0)
+            {
+                throw new ArgumentOutOfRangeException("number2", "Parameters can not be zero.");
+            }
+
+            int modulo = number1 % number2;
+
+            if (modulo == 0)
+            {
+                return number2;
+            }
+
+            return GreatestCommonDivisor(number2, modulo);
         }
         static public bool IsPrime(int number)
         {
-            throw new NotImplementedException();
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException("number", "IsPrime method is working only for positive numbers.");
+            }
+
+            if (number < 2)
+            {
+                return false;
+            }
+
+            int uplimit = ((int)Math.Sqrt(number)) + 1;
+            for (int divisor = 2; divisor < uplimit; divisor++)
+            {
+                if ((number % divisor) == 0)
+                {
+                    return false;
+                }
+
+            }
+            return true;
         }
 
     }
