976ed57e3b219097a41ea8937e3943e922ae2d2e
George Xiros <geo.xiros@gmail.com>
2018-11-17 11:03:38 +0200

    Make userinput Abstract class

--- Project2a/UserInput.cs
+++ Project2a/UserInput.cs
@@ -6,14 +6,17 @@ using System.Threading.Tasks;
 
 namespace Project2a
 {
-    class UserInput
+    abstract class UserInput<T>
     {
         public delegate string GetInput();
         protected GetInput _inputMethod;
 
-        public UserInput(GetInput inputMethod)
+        public bool GetUserInput(string message, out T output)
         {
-            _inputMethod = inputMethod;
+            Console.Write(message);
+            string input = _inputMethod();
+            return IsValidInput(input, out output);
         }
+        public abstract bool IsValidInput(string input, out T output);
     }
 }
