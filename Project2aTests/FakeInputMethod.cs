58a8a12c027b67684f0e434d45365d7b01832740
George Xiros <geo.xiros@gmail.com>
2018-11-18 20:44:27 +0200

    Better FakeInputMethod using array

--- Project2aTests/FakeInputMethod.cs
+++ Project2aTests/FakeInputMethod.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,19 +7,23 @@ using Project2a;
 
 namespace Project2aTests
 {
-    class FakeInputMethod
+  class FakeInputMethod
+  {
+    private readonly string[] _inputs;
+    private int _inputIndex;
+    public FakeInputMethod(string inputs)
     {
-        private List<string> _inputs;
-        public FakeInputMethod(string inputs)
-        {
-            _inputs = inputs.Split(',').ToList<string>();
-        }
-
-        public string GetInput()
-        {
-            string input = _inputs[0].Trim();
-            _inputs.Remove(input);
-            return input;
-        }
+      _inputs = inputs.Split(',');
     }
+
+    public string GetInput()
+    {
+      if (_inputIndex >= _inputs.Length)
+      {
+        _inputIndex = 0;
+      }
+
+      return _inputs[_inputIndex++].Trim();
+    }
+  }
 }
